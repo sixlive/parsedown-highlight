fddb5a123592c9c469c3ffa3ec6e94d1c019a6e9
sixlive <tj@tjmiller.co>
2018-11-07 07:17:07 -0500

    wip

---
+++ examples/jigsaw/README.md
@@ -0,0 +1,11 @@
+# Jigsaw Example
+
+## Setup
+```bash
+> composer install
+> jigsaw build
+> jigsaw serve
+```
+
+After setup, pop open a browser and visit the URL that jigsaw is served at `/test-post`.
+
---
+++ examples/jigsaw/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+
+use TightenCo\Jigsaw\Jigsaw;
+
+/** @var $container \Illuminate\Container\Container */
+/** @var $events \TightenCo\Jigsaw\Events\EventBus */
+
+use Mni\FrontYAML\Markdown\MarkdownParser;
+
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/Parser.php';
+
+$container->bind(MarkdownParser::class, Parser::class);
+
+/**
+ * You can run custom code at different stages of the build process by
+ * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
+ *
+ * For example:
+ *
+ * $events->beforeBuild(function (Jigsaw $jigsaw) {
+ *     // Your code here
+ * });
+ */
