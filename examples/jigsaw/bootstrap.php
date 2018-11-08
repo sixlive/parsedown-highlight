92e5e6f5d671d6d3681896c9fbfde8907b52ea5d
sixlive <tj@tjmiller.co>
2018-11-08 13:05:25 -0500

    styles

--- examples/jigsaw/bootstrap.php
+++ examples/jigsaw/bootstrap.php
@@ -6,13 +6,12 @@ use Mni\FrontYAML\Markdown\MarkdownParser;
 require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/Parser.php';
 
-/** @var $container \Illuminate\Container\Container */
-/** @var $events \TightenCo\Jigsaw\Events\EventBus */
-
+/* @var $container \Illuminate\Container\Container */
+/* @var $events \TightenCo\Jigsaw\Events\EventBus */
 
 $container->bind(MarkdownParser::class, Parser::class);
 
-/**
+/*
  * You can run custom code at different stages of the build process by
  * listening to the 'beforeBuild', 'afterCollections', and 'afterBuild' events.
  *
